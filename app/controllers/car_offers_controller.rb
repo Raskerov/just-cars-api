5e50b046bc6be62cb15d5e8b14bbbfcbacbf62d0
Raskerov <robertsikora1997@gmail.com>
2019-11-07 16:35:42 +0100

    Basic controller for car offers

--- app/controllers/car_offers_controller.rb
+++ app/controllers/car_offers_controller.rb
@@ -1,2 +1,30 @@
 class CarOffersController < ApplicationController
+  before_action :set_car_offer, only: :show
+
+  def index
+    # TODO: Include photos
+    car_offers = CarOffer.all
+    render json: car_offers
+  end
+
+  def show
+    # TODO: Include photo
+    render json: @car_offer
+  end
+
+  def create
+    # TODO: Attach photo
+    @car_offer = CarOffer.create!(car_offer_params)
+    render json: @car_offer
+  end
+
+  private
+
+  def set_car_offer
+    @car_offer = CarOffer.find(params[:id])
+  end
+
+  def car_offer_params
+    params.permit(:title, :description, :price, :photo)
+  end
 end
