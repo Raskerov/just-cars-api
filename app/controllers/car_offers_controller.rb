0a02d66cac2193c23cd5928865c97527a6e19840
Raskerov <robertsikora1997@gmail.com>
2019-11-10 12:35:57 +0100

    Attach photo in POST create car offer

--- app/controllers/car_offers_controller.rb
+++ app/controllers/car_offers_controller.rb
@@ -2,6 +2,7 @@ class CarOffersController < ApplicationController
   before_action :set_car_offer, only: :show
 
   def index
+    # TODO: Handle pagination and searching
     car_offers = CarOffer.all
     render json: car_offers
   end
@@ -11,9 +12,13 @@ class CarOffersController < ApplicationController
   end
 
   def create
-    # TODO: Attach photo
-    @car_offer = CarOffer.create!(car_offer_params)
-    render json: @car_offer
+    car_offer = CarOffer.new(car_offer_params)
+    car_offer.photo.attach(
+      io: car_offer_photo_path,
+      filename: car_offer_photo_name
+    )
+    car_offer.save!
+    render json: car_offer
   end
 
   private
@@ -24,5 +29,15 @@ class CarOffersController < ApplicationController
 
   def car_offer_params
     params.permit(:title, :description, :price, :photo)
+  end
+
+  def car_offer_photo_path
+    uri = URI.parse(params[:photo])
+    file = Net::HTTP.get_response(uri).body
+    StringIO.new(file)
+  end
+
+  def car_offer_photo_name
+    File.basename(URI.parse(params[:photo]).path)
   end
 end
