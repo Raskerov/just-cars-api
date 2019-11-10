1c147a0b95e4c16f6f005fed0461695384222c2c
Raskerov <robertsikora1997@gmail.com>
2019-11-10 21:31:41 +0100

    Include photo only when it's present

--- app/serializers/car_offer_serializer.rb
+++ app/serializers/car_offer_serializer.rb
@@ -4,6 +4,7 @@ class CarOfferSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :price, :car_photo_url
 
   def car_photo_url
+    return unless object.photo.present?
     variant = object.photo.variant(resize: "800x600")
     rails_representation_url(variant, only_path: true)
   end
