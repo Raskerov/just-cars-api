03e1c8b26ce5d4c09d35e1bce29cad11654564d1
Raskerov <robertsikora1997@gmail.com>
2019-11-11 17:41:46 +0100

    Car offer filtering

--- app/controllers/api/v1/car_offers_controller.rb
+++ app/controllers/api/v1/car_offers_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::CarOffersController < ApplicationController
   before_action :set_car_offer, only: :show
+  before_action :filter_car_offers, only: :index
 
   def index
-    # TODO: Handle filtering
-    objects_to_display = CarOffer.page(params[:page] ? params[:page].to_i : 1)
+    objects_to_display = @car_offers.page(params[:page] ? params[:page].to_i : 1)
     json_response(objects: objects_to_display, meta: pagination_meta(objects_to_display))
   end
 
@@ -41,6 +41,15 @@ class Api::V1::CarOffersController < ApplicationController
 
   def offer_photo_name
     File.basename(URI.parse(params[:photo]).path)
+  end
+
+  def filter_car_offers
+    offers = CarOffer.all
+    offers = offers.in_title(params[:in_title]) if params[:in_title].present?
+    offers = offers.in_desc(params[:in_desc]) if params[:in_desc].present?
+    offers = offers.min_price(params[:min_price]) if params[:min_price].present?
+    offers = offers.max_price(params[:max_price]) if params[:max_price].present?
+    @car_offers = offers
   end
 
   def pagination_meta(object)
--- app/models/car_offer.rb
+++ app/models/car_offer.rb
@@ -4,4 +4,9 @@ class CarOffer < ApplicationRecord
   validates_presence_of :title, :description, :price
 
   paginates_per 10
+
+  scope :in_title, ->(query) { where('title LIKE ?', '%' + query + '%') }
+  scope :in_desc, ->(query) { where('description LIKE ?', '%' + query + '%') }
+  scope :min_price, ->(query) { where('price > ?', query) }
+  scope :max_price, ->(query) { where('price < ?', query) }
 end
